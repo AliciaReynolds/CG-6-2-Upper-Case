ea5e7988e16b60d1a3b8c2a7220f457845b62e89
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-08-16 22:57:20 -0500

    Adding comments

--- CG 6-2 Upper Case/Program.cs
+++ CG 6-2 Upper Case/Program.cs
@@ -6,9 +6,17 @@ namespace CG_6_2_Upper_Case
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please write a favortie quote.");        
-            string myString = Console.ReadLine();
+            /*This is code asking for the user to input a sentence for the program.*/
+            Console.WriteLine("Please write a favortie quote.");
+
+            //This is taking the inputted data and assigning a variable
+            string myString = Console.ReadLine();        
+
+            //This is taking the inputted variable and turning it into a variable
+            //in all upper case
             string myUString = myString.ToUpper();
+
+            //This is writing said upper case sentence.
             Console.WriteLine(myUString);
 
             Console.ReadLine();
